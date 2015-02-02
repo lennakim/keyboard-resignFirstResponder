0b7e4c913bcdbdb29f36f10b6b34bdb0eb5daca9
shooter <shooter.hero@gmail.com>
2015-02-02 18:33:11 +0800

    取消键盘

--- keyboard-resignFirstResponder/ViewController.swift
+++ keyboard-resignFirstResponder/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var nameTxt: UITextField!
+  @IBOutlet weak var resultLab: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    nameTxt.placeholder = "邹倩雯"
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+  
+  override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+    nameTxt.resignFirstResponder() //当手指离开屏幕 取消键盘
+  }
+  
+  @IBAction func okTapped(sender: AnyObject) {
+    nameTxt.resignFirstResponder() //取消键盘
+    
+    resultLab.text = "hi, \(nameTxt.text)"
+  }
+  
 
 }
 
